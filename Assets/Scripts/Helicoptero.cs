dd444f2ce470b191a0297ad7ac97fd36d8e3b805
Paulo Salvatore <paulo@salvatore.tech>
2019-05-14 09:38:11 -0300

    Projeto finalizado

--- Assets/Scripts/Helicoptero.cs
+++ Assets/Scripts/Helicoptero.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,13 @@ public class Helicoptero : MonoBehaviour
 {
     FollowTarget followTarget;
 
-	void Start()
+    public GameObject explosionPrefab;
+
+    public Rigidbody rb;
+
+    public float forcaTorque = 200f;
+
+    void Start()
     {
         followTarget = GetComponent<FollowTarget>();
 	}
@@ -17,11 +24,35 @@ public class Helicoptero : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        //print("trigger enter: " + other.tag);
         if (other.CompareTag("Waypoint"))
         {
             Waypoint waypoint = other.GetComponent<Waypoint>();
             Waypoint waypointPosterior = waypoint.waypointPosterior;
             followTarget.target = waypointPosterior.transform;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //print("collision enter: " + other.gameObject.tag);
+        if (other.gameObject.CompareTag("Projétil"))
+        {
+            //print("É um projétil");
+
+            rb.isKinematic = false;
+            rb.useGravity = true;
+
+            Destroy(other.gameObject);
+
+            Instantiate(explosionPrefab, transform.position, explosionPrefab.transform.rotation);
+
+            rb.AddTorque(Vector3.right * forcaTorque);
+        }
+        else if (other.gameObject.CompareTag("Floor"))
+        {
+            Destroy(gameObject);
+            Instantiate(explosionPrefab, transform.position, explosionPrefab.transform.rotation);
         }
     }
 }
